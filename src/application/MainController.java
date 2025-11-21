30430920100e1187048b8bfaafd4ffd0289ef2ec
Rodrigo de Sousa <rdsousa91@gmail.com>
2025-11-21 15:25:08 -0300

    fix: update version label to 7.0

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -114,7 +114,7 @@ public class MainController implements Initializable {
         batteryComboBox.getSelectionModel().selectedItemProperty()
                 .addListener((observable, oldValue, newValue) -> showResults());
 
-        versaoLabel.setText("6.5");
+        versaoLabel.setText("7.0");
     }
 
     @FXML
